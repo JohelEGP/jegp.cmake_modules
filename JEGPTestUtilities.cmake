5b6f1abc5e7f5ab14021ebccf7dda9de48b8d3de
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2019-06-26 21:23:43 -0400

    Update module names

--- JEGPTestUtilities.cmake
+++ JEGPTestUtilities.cmake
@@ -1,2 +1,2 @@
-include(${CMAKE_CURRENT_LIST_DIR}/jegp_add_header_test.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/jegp_add_test.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/JEGPAddHeaderTest.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/JEGPAddTest.cmake)
