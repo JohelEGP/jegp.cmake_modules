46c402e890c4febffb3c231e58748e43be5cc577
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2021-08-23 12:19:37 -0400

    refactor: check for correct usage

--- .detail/JEGPDefineVariables.cmake
+++ .detail/JEGPDefineVariables.cmake
@@ -1,3 +1,7 @@
+if(NOT DEFINED CMAKE_CURRENT_FUNCTION AND NOT DEFINED CMAKE_SCRIPT_MODE_FILE)
+  message(FATAL_ERROR "include() this module inside a function or script.")
+endif()
+
 macro(_jegp_define_variable variable default)
   if(NOT DEFINED "${variable}")
     set("${variable}" "${default}")
