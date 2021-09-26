3f3f7e31fd66326864e5fbdd3a1b7ef27e711573
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2021-09-26 10:19:36 -0400

    refactor: use file() rather than CMake CLI mode

--- modules/.detail/SymlinkClangObjectAsPCM.cmake
+++ modules/.detail/SymlinkClangObjectAsPCM.cmake
@@ -1,5 +1,4 @@
 cmake_path(GET INPUT PARENT_PATH input_dir)
 cmake_path(GET OUTPUT PARENT_PATH output_dir)
-execute_process(COMMAND "${CMAKE_COMMAND}" -E make_directory "${input_dir}"
-                COMMAND "${CMAKE_COMMAND}" -E make_directory "${output_dir}" COMMAND_ERROR_IS_FATAL ANY)
-execute_process(COMMAND "${CMAKE_COMMAND}" -E create_symlink "${INPUT}" "${OUTPUT}" COMMAND_ERROR_IS_FATAL ANY)
+file(MAKE_DIRECTORY "${input_dir}" "${output_dir}")
+file(CREATE_LINK "${INPUT}" "${OUTPUT}" SYMBOLIC)
