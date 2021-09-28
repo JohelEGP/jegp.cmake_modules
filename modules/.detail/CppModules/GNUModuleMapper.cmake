f13e0c47d1ece0b8ded0d20a88440c1f2ee26ad8
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2021-09-28 09:24:50 -0400

    refactor: drop redundant filename suffix

--- modules/.detail/CppModules/GNUModuleMapper.cmake
+++ modules/.detail/CppModules/GNUModuleMapper.cmake
@@ -1,4 +1,4 @@
-set(jegp_gnu_module_mapper_file "${CMAKE_BINARY_DIR}/module_mapper_file.txt")
+set(jegp_gnu_module_mapper_file "${CMAKE_BINARY_DIR}/module_mapper.txt")
 set(jegp_gnu_module_mapper_option "-fmodule-mapper=${jegp_gnu_module_mapper_file}")
 
 function(jegp_gnu_module_mapper_add_mapping module_name gcm_filename #[[module_mapper_file]])
