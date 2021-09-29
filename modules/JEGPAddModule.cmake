e3eae79186a877995b31e8509743a253045fc16a
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2021-09-29 03:17:17 -0400

    fix: support intra-module linking for Clang

--- modules/JEGPAddModule.cmake
+++ modules/JEGPAddModule.cmake
@@ -7,21 +7,16 @@ function(jegp_add_module name)
   include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/.detail/JEGPDefineVariables.cmake")
 
   set(script_dir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/.detail/CppModules")
-
-  if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    set(object_name "_jegp_module_object_${JEGP_${PROJECT_NAME}_NAME_PREFIX}${name}")
-    set(pcm "${CMAKE_CURRENT_BINARY_DIR}/PCM/${name}.pcm")
-
-    _jegp_set_script_directory("${script_dir}")
-    _jegp_set_script_command(SymlinkClangObjectAsPCM "INPUT=$<TARGET_OBJECTS:${object_name}>" "OUTPUT=${pcm}")
-    add_custom_command(OUTPUT "${pcm}" COMMAND ${SymlinkClangObjectAsPCM})
-  endif()
+  set(pcm_file "${CMAKE_CURRENT_BINARY_DIR}/PCM/${name}.pcm")
+  set(pcm_name "_jegp_module_object_${JEGP_${PROJECT_NAME}_NAME_PREFIX}${name}")
 
   _jegp_add_target(
     ${name}
     TYPE OBJECT_LIBRARY
-    SOURCES "$<$<CXX_COMPILER_ID:GNU>:${_SOURCES}>" ${pcm}
-    COMPILE_OPTIONS ${_COMPILE_OPTIONS}
+    SOURCES ${_SOURCES}
+    COMPILE_OPTIONS
+      ${_COMPILE_OPTIONS} PUBLIC $<$<CXX_COMPILER_ID:Clang>:-fmodules -fbuiltin-module-map> INTERFACE
+      $<$<NOT:$<STREQUAL:${pcm_name},$<TARGET_PROPERTY:NAME>>>:$<$<CXX_COMPILER_ID:Clang>:-fmodule-file=${pcm_file}>>
     LINK_LIBRARIES
       ${_LINK_LIBRARIES} INTERFACE
       $<$<CXX_COMPILER_ID:GNU>:$<$<NOT:$<IN_LIST:${name},$<TARGET_PROPERTY:LINK_LIBRARIES>>>:$<TARGET_OBJECTS:${name}>>>
@@ -38,15 +33,18 @@ function(jegp_add_module name)
 
     set_source_files_properties(${_SOURCES} PROPERTIES OBJECT_OUTPUTS "${gcm_filename}")
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    add_library(${object_name} OBJECT "${_SOURCES}")
-    add_dependencies(${name} ${object_name})
+    _jegp_add_target(
+      ${pcm_name}
+      TYPE OBJECT_LIBRARY
+      SOURCES ${_SOURCES}
+      COMPILE_OPTIONS ${_COMPILE_OPTIONS} PRIVATE -Xclang -emit-module-interface
+      LINK_LIBRARIES ${_LINK_LIBRARIES} PUBLIC ${name})
 
-    macro(common_target_calls target)
-      target_compile_options(${target} PUBLIC -fmodules -fbuiltin-module-map PRIVATE -Xclang -emit-module-interface
-                             INTERFACE "-fmodule-file=${pcm}")
-    endmacro()
+    _jegp_set_script_directory("${script_dir}")
+    _jegp_set_script_command(SymlinkClangObjectAsPCM "INPUT=$<TARGET_OBJECTS:${pcm_name}>" "OUTPUT=${pcm_file}")
 
-    common_target_calls(${name})
-    common_target_calls(${object_name})
+    add_custom_command(OUTPUT "${pcm_file}" COMMAND ${SymlinkClangObjectAsPCM} DEPENDS $<TARGET_OBJECTS:${pcm_name}>)
+
+    target_sources(${name} INTERFACE "${pcm_file}")
   endif()
 endfunction()
