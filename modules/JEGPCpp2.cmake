b1c5292757892b4cf5610c9c6713f0605005e12f
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2023-03-09 22:22:27 -0400

    refactor: write Cpp1 to binary directory

--- modules/JEGPCpp2.cmake
+++ modules/JEGPCpp2.cmake
@@ -5,11 +5,17 @@ function(jegp_cpp2_target name)
   list(FILTER cpp2_sources INCLUDE REGEX "\\.cpp2$")
 
   foreach(cpp2_src IN LISTS cpp2_sources)
-    get_source_file_property(cpp2_src "${cpp2_src}" LOCATION)
     string(REGEX REPLACE "2$" "" cpp1_src "${cpp2_src}")
 
-    add_custom_command(OUTPUT "${cpp1_src}" COMMAND "${JEGP_CXX2_COMPILER}" "${JEGP_CXX2_FLAGS}" "${cpp2_src}"
-                       DEPENDS "${cpp2_src}" WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+    cmake_path(GET cpp1_src PARENT_PATH cpp1_src_parent_path)
+    cmake_path(GET cpp1_src FILENAME cpp1_src_filename)
+    string(PREPEND cpp1_src_parent_path "${CMAKE_CURRENT_BINARY_DIR}/JEGPCpp2/generated_sources/")
+    file(MAKE_DIRECTORY "${cpp1_src_parent_path}")
+    set(cpp1_src "${cpp1_src_parent_path}/${cpp1_src_filename}")
+
+    add_custom_command(
+      OUTPUT "${cpp1_src}" COMMAND "${JEGP_CXX2_COMPILER}" "${JEGP_CXX2_FLAGS}" "${cpp2_src}" -o "${cpp1_src}"
+      DEPENDS "${cpp2_src}" WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
     target_sources("${name}" PRIVATE "${cpp1_src}")
     set_source_files_properties("${cpp1_src}" PROPERTIES INCLUDE_DIRECTORIES "${JEGP_CPPFRONT_INCLUDE_DIRECTORIES}")
   endforeach()
