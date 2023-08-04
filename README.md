666441ef8698eed0b3555987a437200dd3c8c602
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2023-08-03 23:42:13 -0400

    feat: add function `jegp_cpp2_target_sources`

--- README.md
+++ README.md
@@ -116,7 +116,7 @@ And then, only for system headers.
 
 #### `JEGPCpp2`
 
-This module defines the following function.
+This module defines the following functions.
 
 ```
 jegp_cpp2_target(<target>)
@@ -126,6 +126,17 @@ This function enables the target to compile its Cpp2 source files.
 
 The target's `SOURCES` property is inspected for Cpp2 source files.
 Corresponding Cpp1 source files are generated and added as sources to the target.
+
+```
+jegp_cpp2_target_sources()
+```
+
+[`target_sources`][] for Cpp2 source files.
+
+For each argument that is a Cpp2 source file,
+sets up the generation of its Cpp1 source file under [`CMAKE_CURRENT_BINARY_DIR`][], and
+replaces it with the generated Cpp1 source file
+before finally forwarding to `target_sources`.
 
 #### `JEGPProjectModules`
 
@@ -255,3 +266,7 @@ This module includes all test modules.
 [`SOURCES` property]: https://cmake.org/cmake/help/latest/prop_tgt/SOURCES.html
 
 [`CMAKE_VERIFY_INTERFACE_HEADER_SETS`]: https://cmake.org/cmake/help/latest/variable/CMAKE_VERIFY_INTERFACE_HEADER_SETS.html
+
+[`target_sources`]: https://cmake.org/cmake/help/latest/command/target_sources.html
+
+[`CMAKE_CURRENT_BINARY_DIR`]: https://cmake.org/cmake/help/latest/variable/CMAKE_CURRENT_BINARY_DIR.html
--- modules/JEGPCpp2.cmake
+++ modules/JEGPCpp2.cmake
@@ -1,23 +1,48 @@
 include("${CMAKE_CURRENT_LIST_DIR}/.detail/JEGPCheckCpp2.cmake")
 
+function(_jegp_cpp2_generate cpp2_src)
+  string(REGEX REPLACE "2$" "" cpp1_src "${cpp2_src}")
+
+  cmake_path(GET cpp1_src PARENT_PATH cpp1_src_parent_path)
+  cmake_path(GET cpp1_src FILENAME cpp1_src_filename)
+  cmake_path(RELATIVE_PATH cpp1_src_parent_path BASE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
+  string(PREPEND cpp1_src_parent_path "${CMAKE_CURRENT_BINARY_DIR}/${JEGP_CPPFRONT_BUILD_DIR}/")
+  file(MAKE_DIRECTORY "${cpp1_src_parent_path}")
+  set(cpp1_src "${cpp1_src_parent_path}/${cpp1_src_filename}")
+  cmake_path(NORMAL_PATH cpp1_src)
+
+  add_custom_command(
+    OUTPUT "${cpp1_src}" COMMAND "${JEGP_CXX2_COMPILER}" "${JEGP_CXX2_FLAGS}" "${cpp2_src}" -o "${cpp1_src}"
+    DEPENDS "${cpp2_src}" WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+  set_source_files_properties("${cpp1_src}" PROPERTIES INCLUDE_DIRECTORIES "${JEGP_CPPFRONT_INCLUDE_DIRECTORIES}")
+
+  return(PROPAGATE cpp1_src)
+endfunction()
+
 function(jegp_cpp2_target name)
   get_target_property(cpp2_sources "${name}" SOURCES)
   list(FILTER cpp2_sources INCLUDE REGEX "\\.(cpp|h)2$")
 
   foreach(cpp2_src IN LISTS cpp2_sources)
-    string(REGEX REPLACE "2$" "" cpp1_src "${cpp2_src}")
-
-    cmake_path(GET cpp1_src PARENT_PATH cpp1_src_parent_path)
-    cmake_path(GET cpp1_src FILENAME cpp1_src_filename)
-    cmake_path(RELATIVE_PATH cpp1_src_parent_path BASE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
-    string(PREPEND cpp1_src_parent_path "${CMAKE_CURRENT_BINARY_DIR}/${JEGP_CPPFRONT_BUILD_DIR}/")
-    file(MAKE_DIRECTORY "${cpp1_src_parent_path}")
-    set(cpp1_src "${cpp1_src_parent_path}/${cpp1_src_filename}")
-
-    add_custom_command(
-      OUTPUT "${cpp1_src}" COMMAND "${JEGP_CXX2_COMPILER}" "${JEGP_CXX2_FLAGS}" "${cpp2_src}" -o "${cpp1_src}"
-      DEPENDS "${cpp2_src}" WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+    _jegp_cpp2_generate("${cpp2_src}")
     target_sources("${name}" PRIVATE "${cpp1_src}")
-    set_source_files_properties("${cpp1_src}" PROPERTIES INCLUDE_DIRECTORIES "${JEGP_CPPFRONT_INCLUDE_DIRECTORIES}")
   endforeach()
+endfunction()
+
+function(jegp_cpp2_target_sources)
+  set(args)
+  foreach(i RANGE 0 ${ARGC})
+    if(NOT (${ARGV${i}} MATCHES "\\.(cpp|h)2$"))
+      list(APPEND args "\${ARGV${i}}")
+      continue()
+    endif()
+
+    _jegp_cpp2_generate("${ARGV${i}}")
+    set(ARGV${i} "${cpp1_src}")
+    list(APPEND args "\${ARGV${i}}")
+  endforeach()
+  cmake_language(
+    EVAL CODE "
+    message(\"${args}\")
+    target_sources(${args})")
 endfunction()
