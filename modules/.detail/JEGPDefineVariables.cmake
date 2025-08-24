6e0a70d400a287fa10f0536db374554b7747ec23
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2025-08-24 10:45:15 -0400

    refactor!: remove `JEGPHeadersTest` module

--- modules/.detail/JEGPDefineVariables.cmake
+++ modules/.detail/JEGPDefineVariables.cmake
@@ -4,9 +4,6 @@ endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/JEGPUtilities.cmake")
 
-_jegp_default_variable("JEGP_CXX_HEADER_FILE_EXTENSIONS" "H;h++;hh;hpp;hxx;h;HPP")
-_jegp_default_variable("JEGP_${PROJECT_NAME}_NAME_PREFIX" "${PROJECT_NAME}_")
-
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
   set("JEGP_CMI_EXTENSION" ".gcm")
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
--- modules/JEGPTestUtilities.cmake
+++ modules/JEGPTestUtilities.cmake
@@ -1,3 +1,2 @@
-include(${CMAKE_CURRENT_LIST_DIR}/JEGPHeadersTest.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/JEGPAddTest.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/JEGPBuildError.cmake)
